c21e9259d112bad452d24eb6f805bc36143c5911
Hayk Martiros <hayk@skydio.com>
2022-01-18 22:48:41 -0800

    [SymForce] Check in generated LCM files
    Check in LCM files just like we do the generated sym files. This
    greatly simplifies the setup.py build because we don't have to
    install a package created from cmake, that is not an extension. We
    should add a test to verify the generated files. I'm also sure we
    can work around this in setup.py if we really don't want to do this.
    
    Topic: symforce/pip_install

---
+++ lcmtypes/gen/python2.7/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+setup(
+  name='lcmtypes',
+  version='0.1',
+  description='lcmtype python bindings (installed by SymForce)',
+  long_description='lcmtype python bindings (installed by SymForce)',
+  author='Skydio, Inc',
+  author_email='hayk@skydio.com',
+  license='None',
+  packages=find_packages(),
+  zip_safe=False,
+)
+    
